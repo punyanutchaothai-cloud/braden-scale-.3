a39a94b0b8aa1568df4596505d9418a42c22761a
Vibesdk <vibesdk-bot@cloudflare.com>
2026-04-25 09:44:48 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add lucide-react framer-motion clsx tailwind-merge"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
